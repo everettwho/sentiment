01389cb3d9b8c72a4c97a595f651972d0dbe5e2f
Everett Hu <ehu@nerdwallet.com>
2019-04-29 20:02:13 -0700

    Updates

---
+++ precache-manifest.22ab7758f7a8a717dc9cf2ab04d2d315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c07f85bdda3f5b505a56",
+    "url": "/sentiment/static/js/runtime~main.31b2dd07.js"
+  },
+  {
+    "revision": "a3eb03aee466b10d514e",
+    "url": "/sentiment/static/js/main.86c4f91d.chunk.js"
+  },
+  {
+    "revision": "ce8c8f5bd5524230f3bf",
+    "url": "/sentiment/static/js/2.af3a8925.chunk.js"
+  },
+  {
+    "revision": "a3eb03aee466b10d514e",
+    "url": "/sentiment/static/css/main.3771c728.chunk.css"
+  },
+  {
+    "revision": "6827ddf4774ad9858433a75e4144beaf",
+    "url": "/sentiment/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sentiment/precache-manifest.5374a71b0b2c4a09457e8411da3be815.js"
+  "/sentiment/precache-manifest.22ab7758f7a8a717dc9cf2ab04d2d315.js"
 );
 
 workbox.clientsClaim();
